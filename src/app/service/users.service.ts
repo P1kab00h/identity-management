9fdb78b3b05d680299aed141ef4ab93aa24b92aa
P1kab00H <nicolas.lefaure@gmail.com>
2022-12-04 12:23:14 +0100

    users.service creation, in order to get the users/user information we use now the service (not directly get the mock data)

---
+++ src/app/service/users.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { LDAP_USERS } from '../model/ldap-mock-data';
+import { UserLdap } from '../model/user-ldap';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UsersService {
+  // Liste des utilisateurs (mock file => ldap-mock-data.ts)
+  users: UserLdap[] = LDAP_USERS;
+
+  getUsers(): Observable<UserLdap[]> {
+    return of(this.users);
+  }
+
+  getUser(login: string): Observable<UserLdap> {
+    return of (this.users.find(user => user.login === login));
+  }
+
+
+  constructor() { }
+}
