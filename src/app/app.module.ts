4fca414da8f52de5937e77e66cc4a958b791e321
P1kab00H <nicolas.lefaure@gmail.com>
2022-12-21 16:24:41 +0100

    td5 en cours, ko ==> material suite à déplacement des services ldap dans ldap-management

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,37 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { LdapManagementModule } from './ldap-management/ldap-management.module';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LdapListComponent } from './ldap-list/ldap-list.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { AppMaterialModule } from './app-material.module';
-import { NavbarComponent } from './navbar/navbar.component';
-import { LdapDetailComponent } from './ldap-detail/ldap-detail.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { LdapEditComponent } from './ldap-edit/ldap-edit.component';
-import { LdapAddComponent } from './ldap-add/ldap-add.component';
-import { AlertComponent } from './share/alert/alert.component';
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    LdapListComponent,
     PageNotFoundComponent,
-    NavbarComponent,
-    //LdapDetailComponent,
-    LdapEditComponent,
-    LdapAddComponent,
-    AlertComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     ReactiveFormsModule,
-    AppRoutingModule,
     BrowserAnimationsModule,
     AppMaterialModule,
+    LdapManagementModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
